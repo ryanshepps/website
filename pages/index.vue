1ab998b55275ccf945dc56b577f1caffd011125c
ryanshepps <sheppardryan@rogers.com>
2021-09-06 15:17:47 -0400

    add first section to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,45 @@
 <template>
-    <h1>Hello world!</h1>
+    <div class="content-container">
+        <div class="section">
+            <h1>Ryan Sheppard</h1>
+            <p>I'm a highly motivated software developer who understands collaboration. <b>I'm not afraid to speak my mind when I'm subject to something that could be done better</b>.</p>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.content-container {
+    display: flex;
+    justify-content: center;
+}
+
+.section {
+    width: 60%;
+    height: 80vh;
+    margin: 10px;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center; 
+    flex-direction: column;
+}
+
+h1 {
+    font-size: 3em;
+    font-weight: 900;
+    text-align: center;
+    color: var(--importantTextColor);
+    margin: 0;
+}
+
+p {
+    font-size: 1.25em;
+    font-weight: 300;
+    text-align: center;
+    margin: 0;
+}
+
+b {
+    font-weight: 700;
+}
+</style>
