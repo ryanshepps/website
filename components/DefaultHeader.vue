da4d9cd7a5aa056c5716a3cf3188b3babb6fc4d1
ryanshepps <sheppardryan@rogers.com>
2021-09-06 13:07:37 -0400

    init DefaultHeader

---
+++ components/DefaultHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="content-container">
+        <NuxtLink class="link" to="/">About</NuxtLink>
+    </div>
+</template>
+
+<style scoped>
+.content-container {
+    max-width: 1280px;
+    margin: auto;
+
+    display: flex;
+    flex-direction: row;
+    height: 60px;
+}
+
+.link {
+    text-decoration: none;
+    color: var(--defaultTextColor);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80px;
+}
+
+.link:hover {
+    color: var(--hoverDefaultTextColor);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <DefaultHeader />
         <Nuxt />
     </div>
 </template>
