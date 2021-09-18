2df2eeb19eb2e371ca70e4e9c8d5518212f1260e
ryanshepps <sheppardryan@rogers.com>
2021-09-18 19:24:39 -0400

    add "My Stack" section to About page

---
+++ components/about/TechStackImages.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="tech-stack-images">
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+            <img src="~/assets/img/techstack/JavaScript.svg" />
+        </a>
+        <a href="https://reactnative.dev/" target="_blank">
+            <img src="~/assets/img/techstack/ReactNative.svg" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="~/assets/img/techstack/Vue.svg" />
+        </a>
+        <a href="https://nuxtjs.org/" target="_blank">
+            <img src="~/assets/img/techstack/Nuxt.svg" />
+        </a>
+        <a href="https://www.amazon.ca/Programming-Language-2nd-Brian-Kernighan/dp/0131103628/ref=sr_1_1?dchild=1&keywords=c+programming+language&qid=1632006279&sr=8-1" target="_blank">
+            <img src="~/assets/img/techstack/C.svg" />
+        </a>
+    </div>
+</template>
+
+<style scoped>
+#tech-stack-images {
+    margin: 10px;
+
+    display: flex;
+    justify-content: center;
+}
+
+a {
+    display: flex;
+    width: 12.5%;
+}
+
+img {
+    margin: 0px 10px;
+    width: 100%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,17 @@
                 <p>I'm a highly motivated software developer who understands collaboration. <b>I'm not afraid to speak my mind when I'm subject to something that could be done better</b>.</p>
             </div>
         </section>
+        <section class="slanted-section">
+            <div class="content-container">
+                <h1 class="white">My Stack</h1>
+                <AboutTechStackImages />
+            </div>
+        </section>
     </div>
 </template>
+
+<script>
+</script>
 
 <style scoped>
 #about-page {
@@ -21,6 +30,21 @@ section {
     width: 60%;
     height: 80vh;
     display: flex;
+    position: relative;
+}
+
+.slanted-section {
+    width: 100%;
+}
+
+.slanted-section::before {
+    content: '';
+    position: absolute;
+    transform: skewY(-3deg) translateY(0.75em);
+    background-color: var(--importantTextColor);
+    width: 100%;
+    height: 100%;
+    z-index: -1;
 }
 
 h1 {
@@ -40,5 +64,9 @@ p {
 
 b {
     font-weight: 700;
+}
+
+.white {
+    color: white;
 }
 </style>
