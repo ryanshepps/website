b14ab56f21d2c4fa65c515371c16407245dfe3d9
ryanshepps <sheppardryan@rogers.com>
2021-12-04 18:06:02 -0500

    add sidebar

--- pages/documentation.vue
+++ pages/documentation.vue
@@ -1,6 +1,27 @@
 <template>
-    <div class="content-container">
-        <h1>Introduction</h1>
-        <p>I'm a software developer with an interest in philosophy.</p>
+    <div id="documentation-page">
+        <div id="sidebar">
+            <NuxtLink 
+                class="sidebar-link"
+                to="/documentation">
+                Introduction
+            </NuxtLink>
+        </div>
+
+        <div class="content-container">
+            <h1>Introduction</h1>
+            <p>I'm a software developer with an interest in philosophy.</p>
+        </div>
     </div>
 </template>
+
+<style scoped>
+#sidebar {
+    position: fixed;
+    height: 100%;
+    width: calc((100% - var(--defaultViewWidth)) / 2);
+    
+    background-color: var(--importantTextColor); 
+}
+</style>
+
