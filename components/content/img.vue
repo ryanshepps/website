69cf42dd185317161a79f6624b305390be318169
ryxn <sheppardryan@rogers.com>
2022-01-13 08:15:32 -0500

    init content-img

---
+++ components/content/img.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <img :src="getSrc()" :width="width" :height="height" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        src: {
+            type: String,
+            required: true,
+        },
+
+        width: {
+            type: [String, Number],
+            default: "auto",
+        },
+
+        height: {
+            type: [String, Number],
+            default: "auto",
+        },
+    },
+
+    methods: {
+        getSrc() {
+            return require(`~/assets/img/${this.src}`);
+        },
+    },
+}
+</script>
