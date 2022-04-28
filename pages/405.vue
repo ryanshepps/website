ca99be3fcb3892305c54731819be12e01bb37534
ryanshepps <sheppardryan@rogers.com>
2022-04-28 18:54:38 -0400

    init 405 page

---
+++ pages/405.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="content-container full-page">
+        <img
+            class="four-oh-five-img"
+            src="~/assets/img/405.svg">
+    </div>
+</template>
+
+<style scoped>
+.full-page {
+    height: calc(100vh - var(--headerHeight));
+    overflow: hidden;
+}
+
+.four-oh-five-img {
+    width: 100%;
+    height: 100%;
+}
+</style>
+
