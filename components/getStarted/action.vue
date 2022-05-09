1f0e95cfd8db1747df2c615f89e63230ed8112f3
ryxn <sheppardryan@rogers.com>
2022-05-08 20:00:52 -0400

    add get started design

---
+++ components/getStarted/action.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="get-started-action">
+        <img :src="getImageSrc(imgSrc)">
+        <div class="text">
+            <h3 class="white-text">
+                {{ title }}
+            </h3>
+            <p class="white-text">
+                <slot />
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import {getImageSrc} from '~/utilities/images.js';
+
+export default {
+    props: {
+        imgSrc: {
+            type: String,
+            required: true,
+        },
+
+        title: {
+            type: String,
+            required: true,
+        },
+    },
+
+    methods: {
+        getImageSrc,
+    },
+};
+</script>
+
+<style scoped>
+.get-started-action {
+    display: flex;
+    width: 80%;
+    height: 150px;
+}
+
+.text {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    
+    height: 100%;
+}
+
+img {
+    margin: 20px;
+}
+</style>
